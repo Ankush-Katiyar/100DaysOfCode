96db01318350e744776db50d4833e41f4d9567eb
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-20 23:06:36 +0530

    Day1.java
    Day1 of 100 days of code.

---
+++ Day1.java
@@ -0,0 +1,9 @@
+public class day1 {
+    public static void main(String[] args) {
+        System.out.println("Hello there! I have started 100 days of code, really excited!!");
+        System.out.println("This println is use to break the string and start it from next line.");
+        System.out.print("Thankyou!/n");
+        System.out.print("Join me on twitter");  
+    }
+}
+//Hello world with concept of output, understanding of print, println, \n.
