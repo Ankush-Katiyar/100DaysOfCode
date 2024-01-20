42d16017b6bdb40ca485282f32c5360b455f045b
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-20 23:45:24 +0530

    Create Day2.java
    Pattern question, basics of variables and datatypes, sum, diff and multiplication and a practice question .

---
+++ Day2.java
@@ -0,0 +1,26 @@
+public class day2 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+        // variables and datatypes
+        // String name = "Ankush"; // non-primitive
+        // int a = 25; // primitive
+
+        // sum of a and b
+        int a = 30;
+        int b = 40;
+        int sum = a + b;
+        System.out.println(sum);
+
+        // x*y/x-y, x=12, y=24 practice question
+        int x = 12;
+        int y = 24;
+        int ans = (x * y) / (x - y);
+        System.out.println(ans);
+
+    }
+
+}
