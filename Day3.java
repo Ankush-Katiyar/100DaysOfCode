c8869e78ec27e6c6bbefabeb35c54ee8d04cbb49
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-21 21:16:02 +0530

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class day3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+      //input of two integers 
+    }
+}
