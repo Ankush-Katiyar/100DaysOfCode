127daa046a4dbfb74a4ab014e3558276b820ac44
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-22 23:52:14 +0530

    Loop.java
    For Loop  basics

---
+++ Loop.java
@@ -0,0 +1,14 @@
+//for loop
+public class loop {
+
+    public static void main(String args[]) {
+        // for (int counter = 0; counter < 28; counter = counter + 1) {
+        // System.out.println("Hello Ankush");
+        // }
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
