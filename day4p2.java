c23d9ac263b31432229e6ce96f406d0b4f808cd5
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-22 23:21:21 +0530

    (press button) day4p2.java
    Q. Message to be displayed according to the button pressed.

---
+++ day4p2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class day4 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        
+        if (button == 1) {
+            System.out.println("Hello");
+        } else if (button == 2) {
+            System.out.println("YO YO");
+        } else if (button == 3) {
+            System.out.println("Bazinga");
+        } else {
+            System.out.println("Invalid");
+
+        }
+     }
+}
