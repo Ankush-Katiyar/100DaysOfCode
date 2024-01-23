cd7b688e305bbcef417632c6fa712ea99fb4ef83
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-23 22:53:13 +0530

    Create patterns.java
    Nested loop: 
    *****
    *****
    *****
    *****

---
+++ patterns.java
@@ -0,0 +1,16 @@
+//Nested Loops
+public class patterns {
+    public static void main(String args[]) {
+
+        int n = 4;
+        int m = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
