9831aa388e8ece4fcc07e4949b540adc91e590b6
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-27 22:32:14 +0530

    day9.java
    basics of array to store value of 3 subjects.

---
+++ day9.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class day9 {
+    // arrays : list of item of same type
+    public static void main(String args[]) {
+
+        int[] marks = new int[3];
+        marks[0] = 97; // English
+        marks[1] = 34; // Chemistry
+        marks[2] = 87; // Physics
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+
+    }
+
+}
