e6a8eb427de4920439d97735ff2f9e1da7860a90
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-29 23:50:45 +0530

    find.java
    Substring practice.

---
+++ find.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class find {
+    public static void main(String args[]) {
+        String sentence = "My name is ANKI";
+        String name = sentence.substring(11, sentence.length());
+        System.out.println(name);
+    }
+}
