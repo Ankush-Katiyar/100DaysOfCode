f4e692c4f3ca151a0beddf86a2cdb7d01e366860
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-29 23:15:59 +0530

    Strings.java
    Day11 : Basic string : Enter your name.

---
+++ Strings.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Strings {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Your name is : " + name);
+    }
+
+}
