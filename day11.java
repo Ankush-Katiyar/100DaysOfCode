a94d5a112d82a429643df94edea557ff99f1a6f1
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-01-29 22:57:25 +0530

    day11.java
    Find location of x in the 2d array.

---
+++ day11.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class day11 {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+
+        // input
+        // rows
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+
+        // output
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (numbers[i][j] == x) {
+                    System.out.println("x Found on the Location (" + i + ", " + j + ")");
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
