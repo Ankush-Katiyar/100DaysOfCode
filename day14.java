cc6b43f24d0c61053fcb2e86eb8892fd1a14b408
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-02-01 23:20:23 +0530

    day14.java
    Bubble sort basics

---
+++ day14.java
@@ -0,0 +1,26 @@
+public class day14 {
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 7, 8, 3, 1, 2 };
+
+        // bubble sort
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+
+    }
+}
