bafa4c9d4819fd5f4e0faaa6cf613cd2cd897123
Ankush Katiyar <89477915+Ankush-Katiyar@users.noreply.github.com>
2024-02-07 23:42:48 +0530

    day20.java
    Tower Of Hanoi

---
+++ day20.java
@@ -0,0 +1,19 @@
+public class day20 {
+
+    public static void towerOfHanoi(int n, String src, String helper, String dest) {
+        if (n == 1) {
+            System.out.println("transfer disk" + n + " from " + src + " to " + dest);
+            return;
+
+        }
+        towerOfHanoi(n - 1, src, dest, helper);
+        System.out.println("transfer disk" + n + " from " + src + " to " + dest);
+        towerOfHanoi(n - 1, helper, src, dest);
+    }
+
+    public static void main(String args[]) {
+        int n = 2;
+        towerOfHanoi(n, "S", "H", "D");
+
+    }
+}
